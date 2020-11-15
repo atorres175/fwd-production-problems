6f689aa9dd45687a7b37e80355ea95e74ddcc7dd
atorres175 <atorres17@hawk.iit.edu>
2020-11-15 13:09:49 -0600

    Turn in Production Problem 4

--- pp-04/site.js
+++ pp-04/site.js
@@ -3,17 +3,17 @@
   call outputs the correct value for x (5). You must still use
   `x` as the variable inside the function, however.
 */
-
-x = 5;
-
+/*
 function double(num) {
   x = num * 2;
   return x;
 }
 
 double(6);
-console.log('The value of x is', x, '-- it should be 5.');
+x = 5;
 
+console.log('The value of x is', x, '-- it should be 5.');
+*/
 /*
   Step 2. Rewrite the JavaScript `double()` function above so that
   it does not pollute the global namespace: that is, neither its
@@ -23,10 +23,39 @@ console.log('The value of x is', x, '-- it should be 5.');
   Be sure to comment out the original `double()` function so you
   can accurately test your work.
 */
+/*
+function main(){
+  function double(num) {
+    x = num * 2;
+    return x;
+  }
 
+  double(6);
+  x = 5;
+
+  console.log('The value of x is', x, '-- it should be 5.');
+}
+main();
+*/
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+function main(){
+  function double(num) {
+    if (num >= 0 || num < 0){
+    x = num * 2;
+    return x;
+    }else{
+    console.log('You must use numerical values')
+  }
+  }
+
+  double(6);
+  x = 5;
+
+  console.log('The value of x is', x, '-- it should be 5.');
+}
+main();
